11c7d653f2d20fbd6fb18a722c97764fa2ab8ac8
furkanx007 <guvencf16@itu.edu.tr>
2019-12-24 19:13:32 +0300

    heroku trying 9

--- heroku_init.py
+++ heroku_init.py
@@ -1,5 +1,8 @@
 def init():
     from django.contrib.auth.models import User
+    from django.contrib.auth import authenticate
 
-    user = User.objects.create_superuser(username='admin', password='adminpw')
-    user.save()
+    admin = authenticate(username='admin', password='adminpw')
+    if admin is None:
+        user = User.objects.create_superuser(username='admin', password='adminpw', email='admin@admin.com')
+        user.save()
